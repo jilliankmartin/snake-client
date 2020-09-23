6254b036953647d8a415f55ab7a4c677386de5e5
jilliankmartin <jillmartin@live.com>
2020-09-23 22:51:16 +0000

    moved to input

---
+++ input.js
@@ -0,0 +1,23 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function() {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  };
+  
+  handleUserInput();
+
+  
+  return stdin;
+}
+
+// setupInput();
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,31 +1,35 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
+
+setupInput();
 
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
 
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-  };
+//   const handleUserInput = function() {
+//     stdin.on('data', (key) => {
+//       if (key === '\u0003') {
+//         process.exit();
+//       }
+//     });
+//   };
   
-  handleUserInput();
+//   handleUserInput();
 
   
-  return stdin;
-}
+//   return stdin;
+// }
 
-setupInput();
+// setupInput();
 
 
