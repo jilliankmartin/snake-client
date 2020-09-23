76724983459c810c665ca6ea8ae3bb30399ffa34
jilliankmartin <jillmartin@live.com>
2020-09-23 21:35:22 +0000

    pushed moves

--- client.js
+++ client.js
@@ -11,16 +11,24 @@ const connect = function() {
 
   conn.on('connect', () => {
     console.log("Successfully connected to game server");
-  });
-
-  conn.on('connect', () => {
     conn.write("Name: JIL");
+    // conn.write("Move: up");
+    // setTimeout(() => {conn.write("Move: up")}, 50);
+    // setTimeout(() => {conn.write("Move: up")}, 100);
+    // setTimeout(() => {conn.write("Move: up")}, 150);
+    // setTimeout(() => {conn.write("Move: up")}, 200);
+    // setTimeout(() => {conn.write("Move: up")}, 250);
+    // conn.write("Move: left");
+    // conn.write("Move: down");
+    // conn.write("Move: right");
   });
 
   conn.setEncoding('utf8'); 
 
   conn.on('data', (data) => {
     console.log('Server says: ', data);
+
+  
   });
 
   return conn;
